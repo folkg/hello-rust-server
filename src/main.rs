c9bab0019ec91fd6886e9b0cafa9cb5c38add847
Graeme Folk <graemefolk@gmail.com>
2023-05-20 14:11:14 -0600

    simple server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::{net::TcpListener, println};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // one stream represents one connection
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+    }
 }
